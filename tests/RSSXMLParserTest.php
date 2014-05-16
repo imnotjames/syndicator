0ea73cf8434d748d1f15cf2aeafbf44493e304f9
James <james@notjam.es>
2014-05-16 01:21:32 -0400

    add RSSXMLParserTest.php and the DataProviderIterator

---
+++ tests/RSSXMLParserTest.php
@@ -0,0 +1,50 @@
+<?php
+
+class RSSXMLParserTest extends PHPUnit_Framework_TestCase {
+	/**
+	 * @return \Iterator
+	 */
+	public function getDataSourceInvalidRSS() {
+		return new DataProviderIterator(
+			new RegexIterator(
+				new RecursiveIteratorIterator(
+					new RecursiveDirectoryIterator(
+						'./tests/feeds/invalid/',
+						FilesystemIterator::SKIP_DOTS
+					)
+				),
+				'/\.xml$/i'
+			)
+		);
+	}
+
+	/**
+	 * @dataProvider getDataSourceInvalidRSS
+	 *
+	 * @param $input
+	 */
+	public function testParse($input) {
+		$xml = file_get_contents($input);
+
+		$expectedException = '\imnotjames\Syndicator\Exceptions\ParsingException';
+
+		if (preg_match('/<!-- exception: (.+) -->/', $xml, $expectedExceptionMatches)) {
+			$expectedException = $expectedExceptionMatches[1];
+		}
+
+		if (preg_match('/<!-- exception message: (.+) -->/', $xml, $expectedExceptionMessageMatches)) {
+			$expectedExceptionMessage = $expectedExceptionMessageMatches[1];
+		}
+
+		if (!empty($expectedExceptionMessage)) {
+			$this->setExpectedException($expectedException, $expectedExceptionMessage);
+		} else {
+			$this->setExpectedException($expectedException);
+		}
+
+		$parser = new \imnotjames\Syndicator\Parsers\RSSXML();
+
+		$parser->parse($xml);
+	}
+}
+
